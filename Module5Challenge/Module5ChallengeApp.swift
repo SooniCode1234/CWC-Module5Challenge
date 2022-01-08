6920c8aab5cc33bcda10530e3735f1567cf1aa19
Sooni Mohammed <sooni.mohaammed@gmail.com>
2022-01-08 17:05:38 -0500

    Initial Commit

---
+++ Module5Challenge/Module5ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module5ChallengeApp.swift
+//  Module5Challenge
+//
+//  Created by Sooni Mohammed on 2022-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Module5ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
